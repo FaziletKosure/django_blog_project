5a12161f095b2a46193aba108c7d4675373065ef
FaziletKosure <faziletkosure1@gmail.com>
2020-12-30 05:32:32 +0100

    add

--- src/myblog/views.py
+++ src/myblog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -21,6 +22,7 @@ class ArticleDetailView(DetailView):
 
 class AddPostView(CreateView):
     model = Post
+    form_class = PostForm
     template_name = 'add_post.html'
-    fields = '__all__'
+    # fields = '__all__'
     # fields=('title','body')
