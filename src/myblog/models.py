477ad86a5a7af96637f79c5c3c7b9184636ce6ac
FaziletKosure <faziletkosure1@gmail.com>
2020-12-30 04:32:30 +0100

    add

--- src/myblog/models.py
+++ src/myblog/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
+    title_tag = models.CharField(
+        max_length=255, default='Welcom To My Awesome Blog!')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
+
+    def get_absolute_url(self):
+        # return reverse('article-detail', args=(str(self.id)))
+        return reverse('home')
--- src/myblog/views.py
+++ src/myblog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 
 from .models import Post
 
@@ -12,3 +12,15 @@ from .models import Post
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_details.html'
+
+
+class AddPostView(CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    fields = '__all__'
+    # fields=('title','body')
