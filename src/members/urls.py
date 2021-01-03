ecc133553187bd5d538c238bb1ac45597549329a
FaziletKosure <faziletkosure1@gmail.com>
2021-01-03 21:20:45 +0100

    add

--- src/members/urls.py
+++ src/members/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from .views import UserRegisterView
+from .views import UserRegisterView, UserEditView
 
 
 urlpatterns = [
-    path('register/', UserRegisterView.as_view(), name='register')
+    path('register/', UserRegisterView.as_view(), name='register'),
+    path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
 
 
 ]
